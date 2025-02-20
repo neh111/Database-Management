6236787f39e06bf1b285304c961f4be9cec3e875
Nechama <hr4563221@gmail.com>
2025-02-20 11:51:44 +0200

    Search case - without any style

--- src/Components/AppStarts.jsx
+++ src/Components/AppStarts.jsx
@@ -4,20 +4,32 @@ import { getAllUsers } from "../Utils/usersUtils";
 
 export default function AppStarts() {
   const [users, setUsers] = useState([]);
+  const [filteredUsers,setFilteredUsers]=useState([]);
   useEffect(() => {
     const getUsersData = async () => {
       const resp = await getAllUsers();
       setUsers(resp);
+      setFilteredUsers(resp);
     };
     getUsersData();
   }, []);
 
+  const handleSearch=(searchStr)=>{
+      console.log(searchStr)
+      const filteredUsers=users.filter(user=>
+        user.name.toLowerCase().includes(searchStr.toLowerCase()) ||
+        user.email.toLowerCase().includes(searchStr.toLowerCase())
+      );
+      console.log(filteredUsers)
+      setFilteredUsers(filteredUsers)
+  }
+
   return (
     <div>
       <label>Search</label>
-      <input type="text"></input>
+      <input type="text" onChange={(event)=>handleSearch(event.target.value)}></input>
       <button>Add</button>
-      {users.map((user) => (
+      {filteredUsers.map((user) => (
         <UserGlobalData key={user.id} user={user} />
       ))}
     </div>
